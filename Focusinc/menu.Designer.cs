78f6bd59c6c5a5b275daa9ddefaa77f84843b8bd
Darell Domalaon <darelldomalaon@gmail.com>
2018-04-24 17:10:03 +0800

    UI update

--- Focusinc/menu.Designer.cs
+++ Focusinc/menu.Designer.cs
@@ -28,19 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.custBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(186)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Adobe Heiti Std R", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(639, 527);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 132);
+            this.button1.TabIndex = 0;
+            this.button1.Text = " ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(185)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(868, 385);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(223, 136);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // custBtn
+            // 
+            this.custBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(80)))), ((int)(((byte)(34)))));
+            this.custBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.custBtn.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.custBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.custBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.custBtn.Location = new System.Drawing.Point(639, 385);
+            this.custBtn.Name = "custBtn";
+            this.custBtn.Size = new System.Drawing.Size(223, 136);
+            this.custBtn.TabIndex = 9;
+            this.custBtn.Text = "EMPLOYEE";
+            this.custBtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.custBtn.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(145)))), ((int)(((byte)(251)))));
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.Location = new System.Drawing.Point(1097, 608);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "LOG OUT";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(156)))), ((int)(((byte)(116)))));
+            this.button4.Location = new System.Drawing.Point(1097, 385);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(205, 217);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "button3";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(96)))), ((int)(((byte)(176)))));
+            this.button5.Location = new System.Drawing.Point(868, 528);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(223, 132);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "button3";
+            this.button5.UseVisualStyleBackColor = false;
             // 
             // menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.custBtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "menu";
             this.Text = "menu";
+            this.Load += new System.EventHandler(this.menu_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button custBtn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
--- Focusinc/menu.cs
+++ Focusinc/menu.cs
@@ -16,5 +16,15 @@ namespace Focusinc
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menu_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
